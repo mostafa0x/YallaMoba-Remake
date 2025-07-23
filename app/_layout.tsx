9efed05649a6c615564ecfbb6ae1578bb99f3609
mostafa0x <dodomido12350@gmail.com>
2025-07-23 03:57:27 -0700

    init commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,22 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import "react-native-reanimated";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [fontsLoaded] = useFonts({
+    Inter: require("../assets/fonts/Inter-Regular.ttf"),
+    "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
+    "Inter-Italic": require("../assets/fonts/Inter-Italic.ttf"),
+    "Inter-BoldItalic": require("../assets/fonts/Inter-BoldItalic.ttf"),
+    "WendyOne-Regular": require("../assets/fonts/WendyOne-Regular.ttf"),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
+  if (!fontsLoaded) {
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack />
+    // <StatusBar style="auto" />
   );
 }
---
+++ constants/Fonts.ts
@@ -0,0 +1,7 @@
+export const Fonts = {
+  Inter: "Inter",
+  InterBold: "Inter-Bold",
+  InterItalic: "Inter-Italic",
+  InterBoldItalic: "Inter-BoldItalic",
+  WendyOneRegular: "WendyOne-Regular",
+};
